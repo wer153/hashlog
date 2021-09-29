ce30de072dc70daef10bf7166f06978f75e6a533
minki <minki@minkiui-MacBookAir.local>
2021-09-29 21:12:57 +0900

    feat: add a serializer and viewset for volabulary

--- vocabulary/views.py
+++ vocabulary/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import serializers
+from rest_framework.permissions import AllowAny
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from vocabulary.models import Vocabulary
+
+
+class VocabularySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Vocabulary
+        fields = (
+            'title',
+            'subtitle',
+            'modified_datetime',
+            'text',
+        )
+
+
+class VocabularyViewSet(ModelViewSet):
+    serializer_class = VocabularySerializer
+    permission_classes = AllowAny,
+    lookup_field = 'title'
