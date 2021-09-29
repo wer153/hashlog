0f188c598a1663473d6ba2d8cb72fc2644f2d17a
minki <minki@minkiui-MacBookAir.local>
2021-09-29 20:51:32 +0900

    feat: define vocabulary model

--- vocabulary/models.py
+++ vocabulary/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Vocabulary(models.Model):
+    title = models.CharField(max_length=100)
+    subtitle = models.CharField(max_length=100)
+    date = models.DateField(auto_now=True)
+    text = models.TextField()
+
+    def __str__(self):
+        return f'{self.title}: {self.subtitle}'
