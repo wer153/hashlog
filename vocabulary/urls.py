c3773af409453a80c8da00a5490ed31515ca7dd7
minki <minki@minkiui-MacBookAir.local>
2021-09-29 21:19:00 +0900

    fix: url path of vocalbulary viewset

--- vocabulary/urls.py
+++ vocabulary/urls.py
@@ -3,4 +3,4 @@ from rest_framework.routers import DefaultRouter
 from vocabulary.views import VocabularyViewSet
 
 router = DefaultRouter()
-router.register(r'unit', VocabularyViewSet, basename='vocabulary')
+router.register(r'vocabulary', VocabularyViewSet, basename='vocabulary')
