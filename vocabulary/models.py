6a2f24136ff7474906532efa8fed3fdca4462c5a
minki <minki@minkiui-MacBookAir.local>
2021-09-29 21:12:30 +0900

    feat: replace created_date with created_datetime and add modified_datetime

--- vocabulary/models.py
+++ vocabulary/models.py
@@ -4,7 +4,8 @@ from django.db import models
 class Vocabulary(models.Model):
     title = models.CharField(max_length=100)
     subtitle = models.CharField(max_length=100)
-    date = models.DateField(auto_now=True)
+    created_datetime = models.DateTimeField(auto_now=True)
+    modified_datetime = models.DateTimeField(auto_now=True)
     text = models.TextField()
 
     def __str__(self):
