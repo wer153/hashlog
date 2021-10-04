37fa06aecef369e6ab7218d67da3269c79064ff7
minki <minki@minkiui-MacBookAir.local>
2021-10-04 17:04:15 +0900

    ADD: functionality of taking control of query parameters to VocabularyViewSet

--- vocabulary/views.py
+++ vocabulary/views.py
@@ -1,4 +1,6 @@
+from django.core.exceptions import FieldDoesNotExist
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 from rest_framework.permissions import AllowAny
 from rest_framework.viewsets import ModelViewSet
 
@@ -19,5 +21,41 @@ class VocabularySerializer(serializers.ModelSerializer):
 class VocabularyViewSet(ModelViewSet):
     serializer_class = VocabularySerializer
     permission_classes = AllowAny,
-    queryset = Vocabulary.objects.all()
     lookup_field = 'title'
+    keywords = 'starred',
+    model = Vocabulary
+
+    def get_queryset(self):
+        self.validate_query_params()
+        return self.model.objects.filter(
+            **self.clean_query_params()
+        )
+
+    def clean_query_params(self):
+        self.validate_keywords()
+        return {
+            key: value
+            for key, value in self.request.query_params.items()
+            if key in self.keywords
+        }
+
+    def validate_keywords(self):
+        for keyword in self.keywords:
+            if not hasattr(self.model(), keyword):
+                raise FieldDoesNotExist(f'{keyword}필드가 존재하지 않습니다.')
+
+    def validate_query_params(self):
+        for field_name in self.keywords:
+            self.validate_field(field_name)
+
+    def validate_field(self, field_name):
+        if field_name in self.request.query_params.keys():
+            try:
+                validator = getattr(self, f'validate_{field_name}')
+            except AttributeError:
+                raise NotImplementedError(f'validate_{field_name}를 구현해주세요.')
+            validator(self.request.query_params[field_name])
+
+    def validate_starred(self, value):
+        if bool(value) not in (True, False):
+            raise ValidationError()
