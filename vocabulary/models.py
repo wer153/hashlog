9e3fa472da46239eb2e238015b54ae71a038a248
minki <minki@minkiui-MacBookAir.local>
2021-10-04 14:19:07 +0900

    ADD: searched_datetime field to Vocabulary model

--- vocabulary/models.py
+++ vocabulary/models.py
@@ -6,6 +6,7 @@ class Vocabulary(models.Model):
     subtitle = models.CharField(max_length=100, db_index=True)
     created_datetime = models.DateTimeField(auto_now=True)
     modified_datetime = models.DateTimeField(auto_now=True)
+    searched_datetime = models.DateTimeField(auto_now=True)
     content = models.TextField()
 
     def __str__(self):
