eb5eb768bacd9b1e46326019eee0c63d15bce3ed
minki <minki@minkiui-MacBookAir.local>
2021-10-22 16:32:44 +0900

    docs: get database info from my.cnf

--- hashlog/settings.py
+++ hashlog/settings.py
@@ -79,11 +79,12 @@ WSGI_APPLICATION = 'hashlog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': os.path.join(BASE_DIR, 'my_cnf'),
+        },
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
